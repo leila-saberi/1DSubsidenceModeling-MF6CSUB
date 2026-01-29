9e7f763c102645ae8f452f4b6234ac25b74c8975
Leila Saberi <LSaberi@intera.com>
2026-01-29 17:56:20 -0600

    long-term sites

--- Y88/prep_data.py
+++ Y88/prep_data.py
@@ -14,7 +14,7 @@ def prep_data(use_delay, **kwargs):
     from dependencies.project_functions.layer_functions import layer_thicknesses
 
     w_d = "."
-    location = "Y88_lay3"
+    location = "Y88"
 
     if os.path.exists("processed_data"):
         shutil.rmtree("processed_data")
